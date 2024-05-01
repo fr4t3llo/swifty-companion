4ba32e2c42ede0ae313d8b4a87603d6443773576
skasmi <skasmi@FRATELLO>
2024-05-01 19:54:45 +0100

    add front (welcome page & search page)

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'search_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _textFieldController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textFieldController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/back.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/logo42.png'),
+                const Text(
+                  'Swifty Companion',
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 50, fontFamily: 'my'),
+                ),
+                const SizedBox(height: 100),
+                SizedBox(
+                  width: 150,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SearchPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      backgroundColor: const Color(0xFFF7941D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'mytwo',
+                            fontSize: 20,
+                          ),
+                        ),
+                        Icon(
+                          Iconsax.login_1,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/search_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/back.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/logo42.png'),
+                const Text(
+                  'Swifty Companion',
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 50, fontFamily: 'my'),
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width - 200,
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(
+                        color: Colors.black, fontFamily: 'mytwo', fontSize: 20),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 255, 255, 255),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width - 250,
+                  height: 45, // Adjust the width as needed
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SearchPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      backgroundColor: const Color(0xFFF7941D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "SEARCH",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'mytwo',
+                            fontSize: 20,
+                          ),
+                        ),
+                        Icon(
+                          Iconsax.search_normal,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
