bf2b03fff354be4d08c1512a1259468f06f3d4ce
FRATELLO <saife.addine123@gmail.com>
2025-05-13 20:15:25 +0100

    update by FRATELLO

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -56,6 +56,7 @@ class _SearchPageState extends State<SearchPage> {
             _errorMessage = 'User not found';
           });
         }
+        
       }
     } catch (e) {
       if (mounted) {
