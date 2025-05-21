53859dab9d212e0669ef7a8a6f7416bad9f23703
FRATELLO <saife.addine123@gmail.com>
2025-05-21 23:56:05 +0100

    update by FRATELLO

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitUp,     
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
